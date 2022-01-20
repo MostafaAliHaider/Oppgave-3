5d753ffdad1c642edb8b143d580ddc5a6b23c148
MostafaAliHaider <90030448+MostafaAliHaider@users.noreply.github.com>
2022-01-20 21:02:40 +0100

    oppgave 0.5

--- javaScript.js
+++ javaScript.js
@@ -1,5 +1,6 @@
 console.log("yo")
 
+//I denne oppgaven jobbet jeg med en medstudent Yaye
 
 //oppgave 1
 //fjern denne teksten med knappen under
@@ -40,8 +41,49 @@ const btn4 = document.getElementById("write-list");
 console.log(btn4, ul);
 
 btn4.addEventListener("click", function(){
-    let myList = ["liste1", "liste2", "liste3"];
+    let myList = ["hey", "how", "u", "doin"];//de ulike punktene
+
+    let li = myList.map(function(liste){
+        //liste parameteret som er i functionen over er den som vil bli plassert som et punkt mellom <li> taggene
+        return `<li> ${liste}</li>`;
+    });
+
+    //under henter vi ul'en fra html og bruker deretter join('') for å ha mellomrom mellom de ulike punktene
+    ul.innerHTML = li.join('');
+
     
-})
+});
 
 //Oppgave 5
+//hjelp fra https://www.w3schools.com/jsref/prop_element_children.asp
+//nytt element
+const div = document.getElementById("placeholder");
+//Id'en over listen vi kan velge fra
+const liste = document.getElementById("select");
+//id for knappen
+const btn5 = document.getElementById("create");
+//id for inputen 
+const innput = document.getElementById("text");
+
+
+btn5.addEventListener('click', function(e){
+    
+    e.preventDefault();
+    if(select.selectedIndex === 0){
+        div.innerHTML += "<h2>" + innput.value + "</h2";
+        innput.value = null;
+    } else if (select.selectedIndex === 1){
+        div.innerHTML += "<h3>" + innput.value + "</h3>";
+        innput.value = null;
+    } else if (select.selectedIndex === 2){
+        div.innerHTML += "<span>" + innput.value + "</span>";
+        innput.value = null;
+    } else if (select.selectedIndex === 3){
+        div.innerHTML += "<p>" + innput.value + "</p>";
+        innput.value = null;
+    }
+
+});
+
+
+
---
+++ test.js
@@ -0,0 +1,11 @@
+// let alder = 22;
+// let bosted = 'oslo';
+
+// bosted = alder;
+// console.log(alder);
+
+const age = 20;
+
+
+
+console.log(age);
